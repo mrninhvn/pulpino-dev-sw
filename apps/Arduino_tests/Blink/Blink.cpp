2dac483595148501fbf55cead0784b941b509d41
mrninhvn <mr.ninh.vn@outlook.com>
2020-12-22 14:10:44 +0700

    Add Blink example

---
+++ apps/Arduino_tests/Blink/Blink.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+#include <main.cpp>
+
+#define LED 8
+
+void setup()
+{
+  pinMode(LED, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(LED, HIGH);
+  delay(5000);
+  digitalWrite(LED, LOW);
+  delay(5000);
+}
